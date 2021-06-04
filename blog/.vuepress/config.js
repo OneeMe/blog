f2c538834e5ff618a2fa243e0fd934222da6b128
onee <onee.me@outlook.com>
2021-06-04 09:03:39 +0800

    chore: complete basic structure, change back to npm

--- blog/.vuepress/config.js
+++ blog/.vuepress/config.js
@@ -6,12 +6,16 @@ module.exports = {
         dateFormat: 'YYYY-MM-DD',
         nav: [
             {
-                text: 'Blog',
+                text: 'Timeline',
                 link: '/',
             },
             {
-                text: 'Tags',
-                link: '/tag/',
+                text: 'Skill',
+                link: '/skill/',
+            },
+            {
+                text: 'About',
+                link: '/about/',
             },
         ]
     }

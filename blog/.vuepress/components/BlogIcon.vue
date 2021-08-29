355ebe7a4db224c20023e0e01acd1804b0cc9bd7
Forelax <forelax@YujieRens-Mac-Mini.local>
2021-08-29 17:38:58 +0800

    feat: 更新 readme

---
+++ blog/.vuepress/components/BlogIcon.vue
@@ -0,0 +1,20 @@
+<template>
+  <a :href="href" target="_blank"> <img :src="url" /> </a>
+</template>
+<script>
+export default {
+  name: "BlogIcon",
+  props: ["name", "href"],
+  computed: {
+    url() {
+      return `/assets/icons/${this.name}.png`;
+    },
+  },
+};
+</script>
+<style scoped>
+img {
+  width: 48px;
+  height: 48px;
+}
+</style>
--- blog/about.md
+++ blog/about.md
@@ -1,3 +1,9 @@
 # About
 
-SwiftGG 成员，现就职于美团，略懂一点 Flutter 和 Web。
+iOS 开发者，[SwiftGG](https://swift.gg/) 成员，现就职于美团，略懂一点 Flutter 和 Web。
+
+你可以在这些地方找到我：
+
+<BlogIcon name="jike" href="/assets/images/about/jike.png"/>
+<BlogIcon name="github" href="https://github.com/OneeMe"/>
+<BlogIcon name="email" href="mailto:onee.me@outlook.com"/>
